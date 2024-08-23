3b515036f60d7c29b483926cbdbd1548ab457410
MatBedoyan <matbedoyan@gmail.com>
2024-08-23 08:54:19 -0300

    PASO 5: Botón "Probar" y apagar otras luces
    =================================================
    HTML
    -------------------------------------------------
    
    - Se agregó un botón para "Probar" el semáforo.
    
    =================================================
    CSS
    -------------------------------------------------
    
    - Se agregó estilo al nuevo botón y se reposicionaron los anteriores.
    
    =================================================
    JS
    -------------------------------------------------
    
    - Se agregó el selector del nuevo botón.
    
    ***NOTA: Se conveniente que antes de iniciar el programa se prueben las luces, que enciendan.***
    - Se agregó una escucha de evento ante un clic para el botón "Probar" para parpadear unas veces todas las luces y apagarlas.
    
    - Se agregaron expresiones para apagar las luces restantes en cada escucha de evento, para que estén apagadas antes de iniciar un nuevo programa.

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ const green = document.querySelector(".green");
 
 const startBtn = document.querySelector(".start");
 const offlineBtn = document.querySelector(".offline");
+const testBtn = document.querySelector(".test");
 const endBtn = document.querySelector(".end");
 
 
@@ -21,17 +22,38 @@ let blinkY = null;
 
 
 startBtn.addEventListener("click", () => {
-    clearInterval(blinkY); // Finalizar intervalos previos.
+    clearInterval(blinkY); // Finalizar intervalos previos de parpadeo de la luz amarilla.
     red.classList.toggle("red-on");
     yellow.classList.remove("yellow-on"); // Apagar la luz roja.
+    green.classList.remove("green-on"); // Apagar la luz roja.
 });
 
 offlineBtn.addEventListener("click", () => {
     clearInterval(blinkY); // Finalizar intervalos previos.
     red.classList.remove("red-on"); // Apagar la luz roja.
+    yellow.classList.remove("yellow-on"); // Apagar la luz roja.
+    green.classList.remove("green-on"); // Apagar la luz roja.
     blinkY = setInterval(()=>{ // Iniciar un intervalo de parpadeo que se ejecuta cada cierto tiempo.
         yellow.classList.toggle("yellow-on"); // Parpadear la luz amarilla.
     },750); // Tres cuartos de segundo.
+});
+
+testBtn.addEventListener("click", () => {
+    clearInterval(blinkY); // Finalizar intervalos previos de parpadeo de la luz amarilla.
+    red.classList.remove("red-on"); // Apagar la luz roja.
+    yellow.classList.remove("yellow-on"); // Apagar la luz roja.
+    green.classList.remove("green-on"); // Apagar la luz roja.
+    const test = setInterval(()=>{ // Setea un intervalo para parpadear las 3 luces cada cierto tiempo.
+        red.classList.toggle("red-on"); // Parpadear la luz roja.
+        yellow.classList.toggle("yellow-on"); // Parpadear la luz amarilla.
+        green.classList.toggle("green-on"); // Parpadear la luz verde.
+    },250); // Un cuarto de segundo.
+    setTimeout(() => { // Finalizado ese cierto tiempo...
+        clearInterval(test); // ...apagar los parpadeos recientes...
+        red.classList.remove("red-on"); // ...y la luz roja,...
+        yellow.classList.remove("yellow-on"); // ...la luz amarilla,...
+        green.classList.remove("green-on"); // ...y la luz verde.
+    },1000); // 1 segundo.
 });
 
 endBtn.addEventListener("click", () => {
